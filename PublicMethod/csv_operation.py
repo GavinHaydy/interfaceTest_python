b927cd194b5a231208bd7f678dc34732df1f0372
bugpz <13558622779@163.com>
2021-11-26 13:58:15 +0800

    fix：增加写入方法

--- PublicMethod/csv_operation.py
+++ PublicMethod/csv_operation.py
@@ -1,17 +1,36 @@
 import csv
 
 
-class ReadCSV:
+class OperationCSV:
+    def __init__(self, path):
+        self.path = path
+
     def get_value(self, row_num, col_num):
-        with open(self, 'r') as file:
+        with open(self.path, 'r') as file:
             reader = csv.reader(file)
             rows = [row for row in reader]
             return rows[row_num][col_num]
+        """
+            from PublicMethod.csv_operation import OperationCSV
 
+            s = OperationCSV('/home/bugpz/文档/test.csv')
+            print(s.get_value(0, 1))
+        """
 
-'''
-    from csv_operation import ReadCSV
-    ReadCSV.get_value(path,row_num,col_num)
-'''
+    def write_value(self, values, headers=None):
+        if headers is None:
+            with open(self.path, 'w', newline='') as file:
+                f_write = csv.writer(file)
+                f_write.writerows(values)
+        elif headers is not None:
+            with open(self.path, 'w', newline='') as file:
+                f_write = csv.writer(file)
+                f_write.writerow(headers)
+                f_write.writerows(values)
+        """
+            from PublicMethod.csv_operation import OperationCSV
 
+            s = OperationCSV('/home/bugpz/文档/new.csv')
+            s.write_value([('1', '18', '测试'), ('2', '20', '没bug')], {'id', 'age', 'name'})
+        """
 

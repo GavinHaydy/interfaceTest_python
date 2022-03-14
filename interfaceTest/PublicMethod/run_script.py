e53421568f51c5e211ba812ea0b61ccb380f0f4f
the-ruffian <13558622779@163.com>
2022-03-14 11:21:50 +0800

    feat：脚本生成适配HTMLTestRunner，修改说明

--- interfaceTest/PublicMethod/run_script.py
+++ interfaceTest/PublicMethod/run_script.py
@@ -1,18 +1,21 @@
+import json
 import os
 import re
 
 
 class Script:
-    def __init__(self, path, file_name, file_name_1):
+    def __init__(self, path, file_name, file_name_1=None):
         """
 
-        :param path: cases_path
-        :param file_name:
+        :param path: 用例路径
+        :param file_name:  生成的文件名
         :param file_name_1:
         """
         self.path = path
         self.file_name = file_name
         self.file_name_1 = file_name_1
+
+    def pytest_script(self):
         par = re.compile(r'api_')
         tags = []
         func_name = []
@@ -56,3 +59,41 @@ class Script:
                     api_path.append(x1)
                     if par.match(x1):
                         file_testcase.write(f'from {self.path}.{j1} import {x1}\n')
+
+    def unittest_script(self, report_file_name, report_title='title', description=''):
+        """
+
+        :param report_file_name: 测试报告文件名
+        :param report_title: 测试报告标题
+        :param description: 测试报告描述
+        :return:
+        """
+        par = re.compile(r'api_')
+        tags = []
+        data = []
+        for item in os.listdir(f'./{self.path}'):
+            tags.append(item)
+        for tag in tags:
+            for func in os.listdir(f'./{self.path}/{tag}'):
+                func = func.replace('.py', '')
+                if par.match(func) is None:
+                    pass
+                else:
+                    import_path = (self.path.replace("../","")+"/"+tag+"/"+func).replace('/','.')
+                    import_value = func.replace('_testcase','')
+                    json_v = json.dumps({"path":import_path,"class_name":import_value})
+                    data.append(json_v)
+        with open(self.file_name, 'w') as testcase_script:
+            testcase_script.write(f'import unittest\n'
+                                  f'from HTMLTestRunner.HTMLTestRunner import HTMLTestRunner\n')
+            for item in data:
+                testcase_script.write(f'from interfaceTest.{json.loads(item)["path"]} import {json.loads(item)["class_name"]}\n')
+            testcase_script.write(f'\n\ntestsuite = unittest.TestSuite()\n')
+            for item in data:
+                testcase_script.write(f'testsuite.addTest(unittest.TestLoader()'
+                                      f'.loadTestsFromTestCase({json.loads(item)["class_name"]}))\n')
+            testcase_script.write(f'with open("{report_file_name}.html", "w", encoding="utf-8") as report:\n'
+                                  f'\t runner = HTMLTestRunner(stream=report,  title="{report_title}"'
+                                  f', description="{description}")\n'
+                                  f'\t runner.run(testsuite)')
+
